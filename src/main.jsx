05ec5650bef22c50d62a04b0eea6121250acbc2c
Shade <kulakaa073@gmail.com>
2025-08-24 17:41:23 +0300

    reverting some changes

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,6 @@ import { BrowserRouter } from 'react-router';
 import App from './App.jsx';
 import { store } from './redux/store.js';
 import './index.css';
-import 'modern-normalize';
-import './styles/reset.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>

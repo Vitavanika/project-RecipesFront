25e24320771ae1ae5ad15c968b514abe14dfb981
Shade <kulakaa073@gmail.com>
2025-08-24 22:29:49 +0300

    adding generic container

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -4,17 +4,19 @@ import styles from './Footer.module.css';
 
 export const Footer = () => {
   return (
-    <div className={styles.container}>
-      <div className={styles.logoContainer}>
-        <svg className={styles.logoIcon} width="32" height="32">
-          <use href="/images/icons.svg#icon-Group-6884"></use>
-        </svg>
-        <p className={styles.logoText}>Tasteorama</p>
+    <div className="container">
+      <div className={styles.container}>
+        <div className={styles.logoContainer}>
+          <svg className={styles.logoIcon} width="32" height="32">
+            <use href="/images/icons.svg#icon-Group-6884"></use>
+          </svg>
+          <p className={styles.logoText}>Tasteorama</p>
+        </div>
+        <p className={styles.descr}>
+          © 2025 CookingCompanion. All rights reserved.
+        </p>
+        <FooterNav />
       </div>
-      <p className={styles.descr}>
-        © 2025 CookingCompanion. All rights reserved.
-      </p>
-      <FooterNav />
     </div>
   );
 };
--- src/component/Layout/Layout.jsx
+++ src/component/Layout/Layout.jsx
@@ -3,11 +3,9 @@ import { Footer } from '../Footer/Footer';
 
 import { Outlet } from 'react-router';
 
-import styles from './Layout.module.css';
-
 export const Layout = () => {
   return (
-    <div className={styles.container}>
+    <div>
       <Header />
       <Outlet />
       <Footer />

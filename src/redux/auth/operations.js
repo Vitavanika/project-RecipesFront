91ff2d3b1433e70c34c2cf11e0d1b23405f125f9
Vitavanika <vitavanika@gmail.com>
2025-08-25 17:49:12 +0200

    Update axios baseURL to include '/api' for correct endpoint routing

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-axios.defaults.baseURL = 'https://project-recipesback.onrender.com/';
+axios.defaults.baseURL = 'https://project-recipesback.onrender.com/api'; ;
 
 // Додає токен до заголовка запитів
 const setAuthHeader = token => {

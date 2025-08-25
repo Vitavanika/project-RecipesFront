d8f5f34ed2683b1b05cd8c209954379b8b71fac7
Vitavanika <vitavanika@gmail.com>
2025-08-25 17:53:04 +0200

    Revert "Update axios baseURL to include '/api' for correct endpoint routing"
    This reverts commit 91ff2d3b1433e70c34c2cf11e0d1b23405f125f9.

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-axios.defaults.baseURL = 'https://project-recipesback.onrender.com/api'; ;
+axios.defaults.baseURL = 'https://project-recipesback.onrender.com/';
 
 // Додає токен до заголовка запитів
 const setAuthHeader = token => {

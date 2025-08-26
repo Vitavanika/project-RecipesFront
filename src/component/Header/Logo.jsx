83f87b214137d6f250dbe9eae9c2651839dbed57
Vitavanika <vitavanika@gmail.com>
2025-08-26 14:17:17 +0200

    add react-hot-toast to  package.json; add NotFoundPage;

--- src/component/Header/Logo.jsx
+++ src/component/Header/Logo.jsx
@@ -1,13 +1,13 @@
-import { Link } from "react-router";
+import { Link } from 'react-router';
 import css from './Logo.module.css';
 
 export const Logo = () => {
-    return (
-      <Link className={css.container} to="/">
-        <svg className={css.logoIcon} width="32" height="30">
-          <use href="/src/images/icons.svg#icon-Group-6884"></use>
-        </svg>
-        <p className={css.logoTitle}>Tasteorama</p>
-      </Link>
-    );
+  return (
+    <Link className={css.container} to="/">
+      <svg className={css.logoIcon} width="32" height="30">
+        <use href="/sprite.svg#icon-Group-6884"></use>
+      </svg>
+      <p className={css.logoTitle}>Tasteorama</p>
+    </Link>
+  );
 };

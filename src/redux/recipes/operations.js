8dab24862f4b1c08072fdd7bc0e6e4221ae28247
Taras <r.taras.t.1@gmail.com>
2025-08-27 14:51:16 +0300

    upd

--- src/redux/recipes/operations.js
+++ src/redux/recipes/operations.js
@@ -1,34 +1,72 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import apiClient from '../../api/apiClient';
+import axios from 'axios';
+// import setAuthHeader from "../auth/operations";
+
+axios.defaults.baseURL = 'https://project-recipesback.onrender.com/api/';
+
+// тимчасово
+
+const setAuthHeader = token => {
+  axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+};
 
 export const fetchOwnRecipes = createAsyncThunk(
   'recipes/getOwn',
   async (_, thunkAPI) => {
     try {
-      const response = await apiClient.get('/recipes');
+      const state = thunkAPI.getState();
+      setAuthHeader(state.auth.token);
+      const response = await axios.get('/recipes');
       return response.data.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.response?.data || error.message);
     }
   }
 );
-      return thunkAPI.rejectWithValue(error.response?.data || error.message);
-    }
-  }
-);
-
 
 export const fetchFavRecipes = createAsyncThunk(
   'recipes/getFavRecipes',
   async (_, thunkAPI) => {
-export const fetchFavRecipes = createAsyncThunk(
-  "recipes/getFavRecipes", 
-  async (_, thunkAPI) => { 
     try {
-      const response = await apiClient.get("/recipes/favorite");
+      const state = thunkAPI.getState();
+      setAuthHeader(state.auth.token);
+      const response = await axios.get('/recipes/favorite');
       return response.data.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.response?.data || error.message);
-    } 
+    }
+  }
+);
+
+export const getFilteredRecipes = createAsyncThunk(
+  'recipes/getFilteredRecipes',
+  async (_, thunkAPI) => {
+    const state = thunkAPI.getState();
+    const searchPhrase = state.filters.searchPhrase;
+    const selectedIngredients = state.filters.selectedIngredients
+      .map(ingredient => `ingredients=${ingredient}`)
+      .join('&');
+    const selectedCategory = state.filters.selectedCategory
+      .map(category => `category=${category}`)
+      .join('&');
+    const currentPage = state.recipes.filteredRecipes.page ?? 1;
+    const perPage = state.recipes.filteredRecipes.perPage ?? 12;
+
+    const queryParams = [];
+    if (searchPhrase) queryParams.push(`searchPhrase=${searchPhrase}`);
+    if (selectedIngredients) queryParams.push(selectedIngredients);
+    if (selectedCategory) queryParams.push(selectedCategory);
+    queryParams.push(`page=${currentPage}`);
+    queryParams.push(`perPage=${perPage}`);
+
+    const requestPath = `/recipes?${queryParams.join('&')}`;
+    console.log('🚀 ~ requestPath:', requestPath);
+
+    try {
+      const response = await axios.get(requestPath);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
   }
 );

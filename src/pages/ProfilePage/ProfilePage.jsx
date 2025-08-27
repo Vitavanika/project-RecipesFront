07bbbbf5ff5d96d249ee8fc1535b9d0920bd6d00
Lili <Liliia_Nel@proton.me>
2025-08-27 19:25:02 +0200

    make own recipes to default render on the page

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,34 +1,40 @@
 //  "/profile/:recipeType"
 import css from "./ProfilePage.module.css";
 import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router";
+import { useNavigate, useParams } from "react-router";
 import { useEffect } from "react";
 
 import { selectError, selectFavRecipes, selectLoading, selectOwnRecipes } from "../../redux/recipes/selectors";
 import { fetchFavRecipes, fetchOwnRecipes } from "../../redux/recipes/operations";
 
 import ProfileNavigation from "../../component/ProfileNavigation/ProfileNavigation";
-// import RecipesList from "../../component/RecipesList/RecipesList";
+import RecipesList from "../../component/RecipesList/RecipesList";
 import LoadMoreBtn from "../../component/LoadMoreBtn/LoadMoreBtn";
-
 
 
 
 const ProfilePage = () => {
 
   const { recipeType } = useParams();
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   const loading = useSelector(selectLoading);
   const error = useSelector(selectError);
   const ownRecipes = useSelector(selectOwnRecipes);
   const favRecipes = useSelector(selectFavRecipes);
 
+  useEffect(() => {
+    if (!recipeType || (recipeType !== "favorites" && recipeType !== "own")) {
+      navigate("/profile/own", { replace: true });
+    }
+  }, [recipeType, navigate]);
+
   useEffect (()=>{
     if (recipeType === "favorites") {
       dispatch(fetchFavRecipes());
-    } else {
+    } else if (recipeType === "own") {
       dispatch(fetchOwnRecipes());
-    }},[recipeType, dispatch])
+    }},[recipeType, dispatch]);
 
 
   const recipeListToRender = recipeType === "favorites" ? favRecipes : ownRecipes;
@@ -39,7 +45,7 @@ const ProfilePage = () => {
       <ProfileNavigation/>
       {loading && <p>Loading...</p>}
       {error && <p>Something went wrong</p>}
-      {/* {!loading && !error && <RecipesList recipes={recipeListToRender}/>} */}
+      {!loading && !error && <RecipesList recipes={recipeListToRender}/>}
       <LoadMoreBtn/>
     </div>
   );

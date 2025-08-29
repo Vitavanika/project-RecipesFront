8bb41676d192acd98a8d44fcc8e6e3c20ab4ddde
Lili <Liliia_Nel@proton.me>
2025-08-29 13:17:56 +0200

    add handling errors

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -4,12 +4,12 @@ import { useDispatch, useSelector } from "react-redux";
 import { useNavigate, useParams } from "react-router";
 import { useEffect } from "react";
 
-import { selectError, selectFavRecipes, selectLoading, selectOwnRecipes } from "../../redux/recipes/selectors";
+import { selectFavError, selectFavRecipes, selectLoading, selectOwnError, selectOwnRecipes } from "../../redux/recipes/selectors";
 import { fetchFavRecipes, fetchOwnRecipes } from "../../redux/recipes/operations";
 
 import ProfileNavigation from "../../component/ProfileNavigation/ProfileNavigation";
 import RecipesList from "../../component/RecipesList/RecipesList";
-import LoadMoreBtn from "../../component/LoadMoreBtn/LoadMoreBtn";
+// import LoadMoreBtn from "../../component/LoadMoreBtn/LoadMoreBtn";
 
 
 
@@ -19,7 +19,8 @@ const ProfilePage = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const loading = useSelector(selectLoading);
-  const error = useSelector(selectError);
+  const errorOwn = useSelector(selectOwnError);
+  const errorFav = useSelector(selectFavError);
   const ownRecipes = useSelector(selectOwnRecipes);
   const favRecipes = useSelector(selectFavRecipes);
 
@@ -38,15 +39,28 @@ const ProfilePage = () => {
 
 
   const recipeListToRender = recipeType === "favorites" ? favRecipes : ownRecipes;
+  const hasNoRecipes = recipeListToRender.length === 0;
+  const currentError = recipeType === "favorites" ? errorFav : errorOwn;
+
+  const getEmptyMessage = () => {
+  if (recipeType === "favorites") {
+    return "You don't have any saved recipes yet. Add some by clicking the save button.";
+  } else if (recipeType === "own") {
+    return "You haven't added your own recipes yet. Click 'Add recipes' to create your first recipe.";
+  } else {
+    return "No recipes to display.";
+  }
+};
 
   return (
     <div>
       <h1 className={css.title}>My profile</h1>
       <ProfileNavigation/>
       {loading && <p>Loading...</p>}
-      {error && <p>Something went wrong</p>}
-      {!loading && !error && <RecipesList recipes={recipeListToRender}/>}
-      <LoadMoreBtn/>
+      {!loading && currentError && <p>Something went wrong</p>}
+      {!loading && !currentError && hasNoRecipes && <p>{getEmptyMessage()}</p>}
+      {!loading && !currentError && !hasNoRecipes && <RecipesList recipes={recipeListToRender}/>}
+      {/* <LoadMoreBtn/> */}
     </div>
   );
 };

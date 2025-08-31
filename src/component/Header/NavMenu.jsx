572695a0e39b740fa9b26b7b12d6140a79b3cda8
kostyaslob <kostyaslob@gmail.com>
2025-08-31 19:14:31 +0300

    fix buildLinkClass for NavMenu

--- src/component/Header/NavMenu.jsx
+++ src/component/Header/NavMenu.jsx
@@ -1,7 +1,7 @@
 import css from "./NavMenu.module.css";
 import clsx from "clsx";
 
-import { NavLink } from "react-router";
+import { NavLink, useLocation } from "react-router";
 import { useSelector } from "react-redux";
 import { getIsLoggedIn } from "../../redux/auth/selectors";
 import { ProfileBlock } from "./ProfileBlock";
@@ -10,10 +10,11 @@ const buildLinkClass = ({ isActive }) => clsx(css.link, isActive && css.active);
 
 export const NavMenu = ({ onClose, layout }) => {
   const isAuth = useSelector(getIsLoggedIn);
+  const location = useLocation();
 
   const items = {
     recipes: <NavLink to="/" className={buildLinkClass} onClick={onClose}>Recipes</NavLink>,
-    myProfile: <NavLink to="profile/my-recipes" className={buildLinkClass} onClick={onClose}>My Profile</NavLink>,
+    myProfile: <NavLink to="profile/own" className={() => clsx(css.link, location.pathname.startsWith("/profile") && css.active)} onClick={onClose}>My Profile</NavLink>,
     addRecipes: <NavLink className={css.item} to="add-recipe" onClick={onClose}>Add Recipes</NavLink>,
     profileBlock: <ProfileBlock onClose={onClose} />,
     login: <NavLink to="auth/login" className={buildLinkClass} onClick={onClose}>Log in</NavLink>,

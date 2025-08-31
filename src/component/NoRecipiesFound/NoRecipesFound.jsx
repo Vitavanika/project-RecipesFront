67a167ec8a135cd7784c8c1521093ed3f6a6cacb
Taras <r.taras.t.1@gmail.com>
2025-08-31 22:27:57 +0300

    add component

---
+++ src/component/NoRecipiesFound/NoRecipesFound.jsx
@@ -0,0 +1,35 @@
+import { useDispatch } from 'react-redux';
+import styles from './NoRecipesFound.module.css';
+import { useSearchParams } from 'react-router';
+import { resetFilters } from '../../redux/filters/slice';
+import { resetHits, setPaginationParams } from '../../redux/recipes/slice';
+
+export default function NoRecipesFound() {
+  const [, setSearchParams] = useSearchParams();
+
+  const dispatch = useDispatch();
+
+  const handleClick = () => {
+    setSearchParams({});
+    dispatch(resetFilters([]));
+    dispatch(setPaginationParams());
+    dispatch(resetHits());
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <h2 className={styles.message}>
+          We’re sorry! We were not able to find a match.
+        </h2>
+        <button
+          type="button"
+          className={styles.resetFiltersButton}
+          onClick={handleClick}
+        >
+          Reset serach and filters
+        </button>
+      </div>
+    </div>
+  );
+}

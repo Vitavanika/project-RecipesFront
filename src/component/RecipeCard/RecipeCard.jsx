26ab8d45b979df7b8a67bc32500a3eedcbf72e50
Kateryna Troyan <ket.tro@gmail.com>
2025-09-03 19:55:56 +0300

    fix btn profilePage

--- src/component/RecipeCard/RecipeCard.jsx
+++ src/component/RecipeCard/RecipeCard.jsx
@@ -1,21 +1,14 @@
-import { useNavigate } from "react-router";
-import { AuthModal } from "../AuthModal/AuthModal";
-import { useFavoriteRecipe } from "../../hooks/useFavoriteRecipe";
-import Loader from "../Loader/Loader";
-import css from "./RecipeCard.module.css";
+import { useNavigate } from 'react-router';
+import { AuthModal } from '../AuthModal/AuthModal';
+import { useFavoriteRecipe } from '../../hooks/useFavoriteRecipe';
+import Loader from '../Loader/Loader';
+import css from './RecipeCard.module.css';
 
-const RecipeCard = ({
-  recipe,  onLearnMore
-}) => {
-  const {
-    saved,
-    isLoading,
-    showAuthModal,
-    setShowAuthModal,
-    toggleSave,
-  } = useFavoriteRecipe(recipe._id);
+const RecipeCard = ({ recipe, onLearnMore, variant }) => {
+  const { saved, isLoading, showAuthModal, setShowAuthModal, toggleSave } =
+    useFavoriteRecipe(recipe._id);
 
-const navigate = useNavigate();
+  const navigate = useNavigate();
 
   const handleLearnMoreClick = () => {
     if (onLearnMore) {
@@ -30,42 +23,47 @@ const navigate = useNavigate();
       {isLoading && (
         <div className={css.cardOverlay}>
           <Loader />
-           </div>)}
-        <img src={recipe.photo} alt={recipe.name} className={css.cardImage} />
-        <div className={css.cardHeader}>
-          <h3 className={css.cardTitle}>{recipe.name}</h3>
-          <div className={css.cardTime}>
-            <svg className={css.cardTimeIcon}  width="16"
-              height="16" >
-              <use href="/sprite.svg#icon-clock" />
-            </svg>
-            <span>{recipe.cookingTime}</span>
-          </div>
         </div>
-        <p className={css.cardDescription}>{recipe.description}</p>
-        <p className={css.cardCaloris}>~{recipe.foodEnergy} cals</p>
+      )}
+      <img src={recipe.photo} alt={recipe.name} className={css.cardImage} />
+      <div className={css.cardHeader}>
+        <h3 className={css.cardTitle}>{recipe.name}</h3>
+        <div className={css.cardTime}>
+          <svg className={css.cardTimeIcon} width="16" height="16">
+            <use href="/sprite.svg#icon-clock" />
+          </svg>
+          <span>{recipe.cookingTime}</span>
+        </div>
+      </div>
+      <p className={css.cardDescription}>{recipe.description}</p>
+      <p className={css.cardCaloris}>~{recipe.foodEnergy} cals</p>
 
-        <div className={css.cardActions}>
-          <button type="button" className={css.learnMoreBtn} onClick={handleLearnMoreClick}>
-            Learn more
-          </button>
-          <button
+      <div className={css.cardActions}>
+        <button
           type="button"
-          onClick={() => toggleSave(() => setShowAuthModal(true))}
-          className={css.favoriteBtn}
-          disabled={isLoading}
-          aria-label={saved ? "Remove from favorites" : "Add to favorites"}
+           className={`${css.learnMoreBtn} ${variant === "own" ? css.learnMoreOwn : ""}`}
+          onClick={handleLearnMoreClick}
         >
-      <svg
-        width="15"
-        height="17"
-        className={saved ? css.unsaveIcon : css.saveIcon}
-      >
-        <use href="/sprite.svg#icon-bookmark"></use>
-      </svg>
-  </button>
-</div>
-
+          Learn more
+        </button>
+        {(variant === "favorites" || variant === "public") && (
+          <button
+            type="button"
+            onClick={() => toggleSave(() => setShowAuthModal(true))}
+            className={css.favoriteBtn}
+            disabled={isLoading}
+            aria-label={saved ? 'Remove from favorites' : 'Add to favorites'}
+          >
+            <svg
+              width="15"
+              height="17"
+              className={saved ? css.unsaveIcon : css.saveIcon}
+            >
+              <use href="/sprite.svg#icon-bookmark"></use>
+            </svg>
+          </button>
+        )}
+      </div>
 
       {showAuthModal && <AuthModal onClick={() => setShowAuthModal(false)} />}
     </div>
@@ -73,4 +71,3 @@ const navigate = useNavigate();
 };
 
 export default RecipeCard;
-
